a80ff4dde9951a18fd2b938b5473a0b0782b7c90
Andriy Gazin <andriy.gazin@gmail.com>
2019-01-08 23:45:08 +0200

    add data parser

---
+++ parse_data.R
@@ -0,0 +1,27 @@
+library(dplyr)
+library(lubridate)
+
+parse_data <- function(){
+  
+  df <- data.frame()
+  
+  files <- list.files('data', full.names = TRUE)
+  
+  for(i in files) {
+    
+    x <- read.csv(i, stringsAsFactors = F)
+    
+    x <- x %>% 
+      select(id, convocation, gender, birthday, last_name, first_name) %>% 
+      mutate(birthday = as.Date(birthday, format = '%d.%m.%Y'),
+             year = year(birthday)) %>% 
+      filter(!is.na(birthday))
+    
+    df <- rbind.data.frame(df, x)
+    
+  }
+  
+  
+  return(df)
+  
+}
